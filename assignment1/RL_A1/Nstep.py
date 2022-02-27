d97b4925a524856ca6fddba295614b749bc1e78e
LiurongLin <64380568+LiurongLin@users.noreply.github.com>
2022-02-27 01:01:04 +0100

    Lily

--- assignment1/RL_A1/Nstep.py
+++ assignment1/RL_A1/Nstep.py
@@ -28,7 +28,11 @@ class NstepQLearningAgent:
                 raise KeyError("Provide an epsilon")
                 
             # TO DO: Add own code
-            a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+            b = np.random.random_sample()
+            if b < epsilon:
+                a = np.random.randint(0, self.n_actions)  # Replace this with correct action selection
+            else:
+                a = argmax(self.Q_sa[s])
             
                 
         elif policy == 'softmax':
@@ -36,7 +40,8 @@ class NstepQLearningAgent:
                 raise KeyError("Provide a temperature")
                 
             # TO DO: Add own code
-            a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+            probs = softmax(self.Q_sa[s], temp)
+            a = np.random.choice(self.n_actions, p=probs)  # Replace this with correct action selection
             
         return a
         
@@ -46,6 +51,27 @@ class NstepQLearningAgent:
         rewards is a list of rewards observed in the episode, of length T_ep
         done indicates whether the final s in states is was a terminal state '''
         # TO DO: Add own code
+        Tep = len(actions)
+        for t in range (Tep):
+            m = np.minimum(self.n, Tep-t)
+            m_states = states[t:t+m]
+            m_actions = actions[t:t+m]
+            m_rewards = rewards[t:t+m]
+            power = np.arange(0, m, 1)
+            if m_rewards[-1]== +35:
+                G_t = np.dot(np.power(self.gamma, power),m_rewards)
+            else:
+                # G_t = 0
+                # for i in range(m):
+                #     element = self.gamma**i*rewards[t+i]+self.(gamma**m)*m*np.max(self.Q_sa[states[t+i]])
+
+                G_t = np.dot(np.power(self.gamma, power),m_rewards)+self.gamma**m*(np.max(self.Q_sa[states[t+m]]))
+
+            self.Q_sa[states[t],actions[t]] = self.Q_sa[states[t],actions[t]] + self.learning_rate*(G_t-self.Q_sa[states[t],actions[t]])
+
+
+
+
         pass
 
 def n_step_Q(n_timesteps, max_episode_length, learning_rate, gamma, 
@@ -56,8 +82,29 @@ def n_step_Q(n_timesteps, max_episode_length, learning_rate, gamma,
     env = StochasticWindyGridworld(initialize_model=False)
     pi = NstepQLearningAgent(env.n_states, env.n_actions, learning_rate, gamma, n)
     rewards = []
-
+    actions = []
+    states = []
+    time_step = 0
     # TO DO: Write your n-step Q-learning algorithm here!
+
+    while time_step<n_timesteps:
+        s = env.reset()
+        states.append(s)
+        for t in range (max_episode_length):
+            a = pi.select_action(s, policy, epsilon, temp)
+            actions.append(a)
+            s_next, r, done = env.step(a)
+            time_step+=1
+            states.append(s_next)
+            rewards.append(r)
+            if done or time_step==n_timesteps:
+                print("done")
+                break
+            else:
+                s = s_next
+        pi.update(states,actions,rewards,done)
+
+
     
     # if plot:
     #    env.render(Q_sa=pi.Q_sa,plot_optimal_policy=True,step_pause=0.1) # Plot the Q-value estimates during n-step Q-learning execution
