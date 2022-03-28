6f40f2aaaffeb5b180119670abfab392a635504c
MGJvanGroeningen <matthijsvangroeningen@gmail.com>
2022-03-28 17:28:33 +0200

    Make arc/lr plots + selection bug fix + don't need to provide budget as arg

--- assignment2/plot_results.py
+++ assignment2/plot_results.py
@@ -5,7 +5,8 @@ import glob
 from Helper import smooth
 
 
-def plot_rewards(rewards, config_labels, budget=10000, save_file=None):
+def plot_rewards(rewards, config_labels, save_file=None):
+    budget = rewards[0].shape[0]
     steps = np.arange(budget)
     smoothing_window = budget // 10 + 1
     n_configs = len(config_labels)
@@ -14,6 +15,7 @@ def plot_rewards(rewards, config_labels, budget=10000, save_file=None):
 
     for i in range(n_configs):
         ax.plot(steps, smooth(rewards[i], smoothing_window), label=config_labels[i])
+        # ax.plot(steps, rewards[i], label=config_labels[i])
     ax.set_xlabel('Step')
     ax.set_ylabel('Mean reward')
     ax.set_title('DQN mean reward progression')
@@ -24,24 +26,24 @@ def plot_rewards(rewards, config_labels, budget=10000, save_file=None):
     plt.close()
 
 
-def saved_array_to_plot_array(save_array, budget=10000):
+def saved_array_to_plot_array(save_array):
     """
     Convert a saved result into an array that can be plotted.
     """
-    rewards_array = np.empty((0, budget))
+    rep_rewards = []
     rep = np.empty(0)
     for i in save_array:
         # A value of 0 indicates the end of a repetition
         if i == 0:
-            rewards_array = np.append(rewards_array, rep[None, ...], axis=0)
+            rep_rewards.append(rep)
             rep = np.empty(0)
         else:
             # Turn the episode reward into a range and append to the repetition array
             rep = np.append(rep, np.arange(1, i + 1), axis=0)
-    return rewards_array
+    return np.array(rep_rewards)
 
 
-def select_runs(save_dir, budget=10000, **kwargs):
+def select_runs(save_dir, **kwargs):
     """
     Select all runs in a save dir that satisfy the conditions given by the kwargs.
     """
@@ -55,67 +57,79 @@ def select_runs(save_dir, budget=10000, **kwargs):
                 select = False
         if select:
             selected_paths.append(run_path)
-    for i in selected_paths:
-        print(i)
-    print(' ')
     arrays = []
     for run_path in selected_paths:
         save_array = np.load(run_path)
-        arrays.append(saved_array_to_plot_array(save_array, budget))
+        arrays.append(saved_array_to_plot_array(save_array))
     return arrays
 
 
 if __name__ == '__main__':
-    results_dir = './results'
+    results_dir = './hp_arc_lr_results'
+    # results_dir = './hp_pol_tus_bs_results'
+
     if not os.path.exists(results_dir):
         os.mkdir(results_dir)
-
-    budget = 30000
 
     # Should contain arrays with shape [budget] which represent the mean of a certain parameter setting
     mean_rewards = []
     labels = []
 
-    # 'selected_runs' is a list containing [n_repetitions, budget] arrays for every run with the given kwargs
+    # 'selected_runs' is a list containing an [n_repetitions, budget] array for every run with the given kwargs
 
-    # for eps in [0.2, 0.1, 0.05]:
-    #     selected_runs = select_runs(results_dir, budget=budget, pol='egreedy', eps=eps)
-    #     mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
-    #     labels.append(f'egreedy_eps={eps}')
-    #
-    # selected_runs = select_runs(results_dir, budget=budget, pol='egreedy', wd=True)
-    # mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
-    # labels.append(f'egreedy_wd')
-    #
-    # for t in [10, 1, 0.1]:
-    #     selected_runs = select_runs(results_dir, budget=budget, pol='softmax', t=t)
-    #     mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
-    #     labels.append(f'softmax_t={t}')
-    #
-    # selected_runs = select_runs(results_dir, budget=budget, pol='softmax', wd=True)
-    # mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
-    # labels.append(f'softmax_wd')
+    for eps in [0.2, 0.1, 0.05]:
+        selected_runs = select_runs(results_dir, pol='egreedy', eps=eps, wd=False)
+        mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
+        labels.append(f'egreedy_eps={eps}')
+
+    selected_runs = select_runs(results_dir, pol='egreedy', wd=True)
+    mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
+    labels.append(f'egreedy_wd')
+
+    for t in [10.0, 1.0, 0.1]:
+        selected_runs = select_runs(results_dir, pol='softmax', t=t, wd=False)
+        mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
+        labels.append(f'softmax_t={t}')
+
+    selected_runs = select_runs(results_dir, pol='softmax', wd=True)
+    mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
+    labels.append(f'softmax_wd')
 
     # for bs in [50, 200, 800]:
-    #     selected_runs = select_runs(results_dir, budget=budget, bs=bs)
+    #     selected_runs = select_runs(results_dir, bs=bs)
     #     mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
     #     labels.append(f'replay buffer size={bs}')
 
     # for tus in [50, 200, 800]:
-    #     selected_runs = select_runs(results_dir, budget=budget, tus=tus)
+    #     selected_runs = select_runs(results_dir, tus=tus)
     #     mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
     #     labels.append(f'target update step={tus}')
 
-    # for arc in ['32', '64', '32_32']:
-    #     selected_runs = select_runs(results_dir, budget=budget, arc=arc)
+    # for arc in ['32_lr', '64_lr', '32_32']:
+    #     selected_runs = select_runs(results_dir, arc=arc)
     #     mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
     #     labels.append(f'Architecture={arc}')
 
-    for lr in [0.01, 0.005, 0.001]:
-        for arc in ['32_lr', '64_lr', '32_32']:
-            selected_runs = select_runs(results_dir, budget=budget, lr=lr, arc=arc)
-            mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
-            labels.append(f'Learning rate={lr}, arc={arc}')
+    # for lr in [0.01, 0.005, 0.001]:
+    #     selected_runs = select_runs(results_dir, lr=lr)
+    #     mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
+    #     labels.append(f'Learning rate={lr}')
+
+    # for lr in [0.01, 0.005, 0.001]:
+    #     for arc in ['32_lr', '64_lr', '32_32']:
+    #         selected_runs = select_runs(results_dir, lr=lr, arc=arc)
+    #         mean_rewards.append(np.mean(np.concatenate(selected_runs), axis=0))
+    #         labels.append(f'Learning rate={lr}, arc={arc}')
+
+    # all_run_paths = glob.glob(os.path.join(results_dir, '*'))
+    # arrays = []
+    # for run_path in all_run_paths:
+    #     save_array = np.load(run_path)
+    #     arrays.append(saved_array_to_plot_array(save_array))
+    # for idx, run in enumerate(arrays):
+    #     mean_rewards.append(np.mean(run, axis=0))
+    #     labels.append(idx)
+    #     print(idx, all_run_paths[idx])
 
     # Plotting
-    plot_rewards(mean_rewards, config_labels=labels, budget=budget, save_file='dqn_rewards')
+    plot_rewards(mean_rewards, config_labels=labels, save_file='dqn_rewards')
