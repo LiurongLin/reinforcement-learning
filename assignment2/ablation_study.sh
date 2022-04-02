d206a13a2cbd0c247e6b19ee0f134d7dceecb9a6
samderegt <regt@strw.leidenuniv.nl>
2022-04-02 18:32:00 +0200

    Fixed bugs of unused hyperparameters and split code into 2 classes

--- assignment2/ablation_study.sh
+++ assignment2/ablation_study.sh
@@ -1,7 +1,7 @@
 #! /bin/sh.
 
 budget=50000
-n_repetitions=32
+n_repetitions=4
 n_cores=4
 
 arc=32_32
@@ -14,9 +14,9 @@ bs=200
 up_step=50
 
 # DQN
-#python3 deepqn_m.py --experience_replay --target_network --budget $budget --n_repetitions $n_repetitions \
-#        --n_cores $n_cores --architecture $arc --learning_rate $lr --target_update_step $up_step --buffer_size $bs \
-#        --policy $pol --epsilon $eps --with_decay $wd
+python3 deepqn_new.py --experience_replay --target_network --budget $budget --n_repetitions $n_repetitions \
+        --n_cores $n_cores --architecture $arc --learning_rate $lr --target_update_step $up_step --buffer_size $bs \
+        --policy $pol --epsilon $eps --with_decay $wd
 
 # DQN - TN
 #python3 deepqn_m.py --experience_replay --budget $budget --n_repetitions $n_repetitions --n_cores $n_cores \
@@ -28,8 +28,8 @@ up_step=50
 #        --with_decay $wd
 
 # DQN - TN - EP
-python3 deepqn_m.py --budget $budget --n_repetitions $n_repetitions --n_cores $n_cores --architecture $arc \
-        --learning_rate $lr --policy $pol --epsilon $eps --with_decay $wd
+#python3 deepqn_m.py --budget $budget --n_repetitions $n_repetitions --n_cores $n_cores --architecture $arc \
+#        --learning_rate $lr --policy $pol --epsilon $eps --with_decay $wd
 
 
 
