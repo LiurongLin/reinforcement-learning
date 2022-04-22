47bdc4deedc332b50c715f39317a4e3b53edf035
LiurongLin <64380568+LiurongLin@users.noreply.github.com>
2022-04-22 11:58:13 +0200

    add savefile

---
+++ assignment2/abalation_study_Lin.sh
@@ -0,0 +1,16 @@
+# DQN
+python3 deepqn_Lin.py --experience_replay --target_network  --policy egreedy
+
+
+# DQN - TN
+python3 deepqn_Lin.py --experience_replay  --policy egreedy
+
+
+# DQN - EP
+python3 deepqn_Lin.py  --target_network  --policy egreedy
+
+
+# DQN - TN - EP
+python3 deepqn_Lin.py  --policy egreedy
+
+
