63c55fa5a7e0378c425f2ec7ced7572ea74af505
LiurongLin <64380568+LiurongLin@users.noreply.github.com>
2022-05-06 16:59:16 +0200

    code

--- assignment3/experiment_n_boot_1.py
+++ assignment3/experiment_n_boot_1.py
@@ -60,50 +60,50 @@ save_results(rewards_per_rep, rewards_filename)
 args_dict['batch_size'] = 10
 '''
 # ---------------------------------------------------------------------
-args_dict['batch_size'] = 50
-
-# args_dict is placed in a list to avoid unpacking the dictionary
-params = args_dict['n_repetitions'] * [[args_dict]]
-
-# Run the repetitions on multiple cores
-pool = Pool(args_dict['n_cores'])
-results_per_rep = pool.starmap(pool_function, params)
-pool.close()
-pool.join()
-
-rewards_per_rep = [results_per_rep[i][0] for i in range(len(results_per_rep))]
-
-# Create filename and directory to save the rewards
-rewards_filename = get_numpy_file(args_dict['results_dir'], args_dict)
-
-# Save the rewards to a .npy file
-save_results(rewards_per_rep, rewards_filename)
-
-args_dict['batch_size'] = 10
-# ---------------------------------------------------------------------
-args_dict['critic_arc'] = (64,64,64)
-
-# args_dict is placed in a list to avoid unpacking the dictionary
-params = args_dict['n_repetitions'] * [[args_dict]]
-
-# Run the repetitions on multiple cores
-pool = Pool(args_dict['n_cores'])
-results_per_rep = pool.starmap(pool_function, params)
-pool.close()
-pool.join()
-
-rewards_per_rep = [results_per_rep[i][0] for i in range(len(results_per_rep))]
-
-# Create filename and directory to save the rewards
-rewards_filename = get_numpy_file(args_dict['results_dir'], args_dict)
-
-# Save the rewards to a .npy file
-save_results(rewards_per_rep, rewards_filename)
-
-args_dict['critic_arc'] = (64,64)
+# args_dict['batch_size'] = 50
+#
+# # args_dict is placed in a list to avoid unpacking the dictionary
+# params = args_dict['n_repetitions'] * [[args_dict]]
+#
+# # Run the repetitions on multiple cores
+# pool = Pool(args_dict['n_cores'])
+# results_per_rep = pool.starmap(pool_function, params)
+# pool.close()
+# pool.join()
+#
+# rewards_per_rep = [results_per_rep[i][0] for i in range(len(results_per_rep))]
+#
+# # Create filename and directory to save the rewards
+# rewards_filename = get_numpy_file(args_dict['results_dir'], args_dict)
+#
+# # Save the rewards to a .npy file
+# save_results(rewards_per_rep, rewards_filename)
+#
+# args_dict['batch_size'] = 10
+# # ---------------------------------------------------------------------
+# args_dict['critic_arc'] = (64,64,64)
+#
+# # args_dict is placed in a list to avoid unpacking the dictionary
+# params = args_dict['n_repetitions'] * [[args_dict]]
+#
+# # Run the repetitions on multiple cores
+# pool = Pool(args_dict['n_cores'])
+# results_per_rep = pool.starmap(pool_function, params)
+# pool.close()
+# pool.join()
+#
+# rewards_per_rep = [results_per_rep[i][0] for i in range(len(results_per_rep))]
+#
+# # Create filename and directory to save the rewards
+# rewards_filename = get_numpy_file(args_dict['results_dir'], args_dict)
+#
+# # Save the rewards to a .npy file
+# save_results(rewards_per_rep, rewards_filename)
+#
+# args_dict['critic_arc'] = (64,64)
 # ---------------------------------------------------------------------
 args_dict['eta'] = 0.1
-
+args_dict['with_entropy'] = True
 # args_dict is placed in a list to avoid unpacking the dictionary
 params = args_dict['n_repetitions'] * [[args_dict]]
 
